6c9143c757e8bfb73873c4a652376c26a847bef8
Krista Chauhan <kristachauhan@gmail.com>
2026-06-04 20:58:30 +0530

    Fix: add missing quotes to Content-Type header string

--- src/services/article.js
+++ src/services/article.js
@@ -12,7 +12,7 @@ export const articleApi = createApi({
         "X-RapidAPI-Host",
         "ai-article-extractor-and-summarizer.p.rapidapi.com"
       );
-      headers.set("Content-Type", application/json);
+      headers.set("Content-Type", "application/json");
 
       return headers;
     },
